e6ced41572a5d516628505544863e5da41c55e9e
Travis Tatsch <travis.tatsch@gmail.com>
2016-10-30 02:40:34 -0500

    Added basic UI functionality to drinking activity, current problem w/floating action button, will fix in morning

--- app/src/main/java/hackathon/pizzapoindexter/uhhackathon/DrinkingActivity.java
+++ app/src/main/java/hackathon/pizzapoindexter/uhhackathon/DrinkingActivity.java
@@ -1,13 +1,56 @@
 package hackathon.pizzapoindexter.uhhackathon;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DrinkingActivity extends AppCompatActivity {
+
+    final String PREFS_NAME = "MyPrefsFile";
+    private TextView calories;
+    private TextView bac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinking);
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+
+        calories = (TextView)findViewById(R.id.calories);
+        bac = (TextView)findViewById(R.id.bac);
+
+        calories.setText(settings.getString("Calories", null));
+        bac.setText(settings.getString("BAC", null));
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab); //button will lead to addDrink
+        //fab.setImageResource(R.drawable.ic_notifications_black_24dp);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.drinking_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.settings:
+                intent = new Intent(this, SettingsActivity.class);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        startActivity(intent);
+        return true;
     }
 }
--- app/src/main/java/hackathon/pizzapoindexter/uhhackathon/MainActivity.java
+++ app/src/main/java/hackathon/pizzapoindexter/uhhackathon/MainActivity.java
@@ -1,11 +1,14 @@
 package hackathon.pizzapoindexter.uhhackathon;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startDrinking(View view){
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        settings.edit().putString("Calories", "0").apply();
+        settings.edit().putString("BAC", "0");
         Intent intent = new Intent(this, DrinkingActivity.class);
         startActivity(intent);
     }
